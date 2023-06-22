47a36d91f7baeec49170c91baa5ac1fbb428424e
Sakil Ahmed <124081558+sakilahmedjoy@users.noreply.github.com>
2023-06-22 09:02:14 +0600

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student{
+        private int id;
+        private String name;
+        private String email;
+        private String contactNumber;
+        private String address;
+
+    }
